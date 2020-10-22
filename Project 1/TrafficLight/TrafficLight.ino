5a33b6dd811643673608bd0dd2507bff8b2fa746
MikeBlackbeard <72194006+MikeBlackbeard@users.noreply.github.com>
2020-10-22 12:38:16 +0200

    Update TrafficLight.ino

--- Project 1/TrafficLight/TrafficLight.ino
+++ Project 1/TrafficLight/TrafficLight.ino
@@ -15,20 +15,40 @@ void loop() {
  }
 
 void changeLights(){
-    // green off, yellow on for 3 seconds
-    digitalWrite(green, LOW);
-    digitalWrite(yellow, HIGH);
-    delay(3000);
-    // turn off yellow, then turn red on for 5 seconds
-    digitalWrite(yellow, LOW);
-    digitalWrite(red, HIGH);
-    delay(5000);
-    // red and yellow on for 3 seconds (red is already on though)
-    digitalWrite(yellow, HIGH);
-    delay(3000);
-    // turn off red and yellow, then turn on green for 5 seconds
-    digitalWrite(yellow, LOW);
+    go_yellow();
+    go_red();
+    go_yellow_red();
+    go_green();
+}
+
+
+// turn off red and yellow, then turn on green for 5 seconds
+void go_green(){
+  	digitalWrite(yellow, LOW);
     digitalWrite(red, LOW);
     digitalWrite(green, HIGH);
     delay(5000);
+}
+
+
+// turn off yellow, then turn red on for 5 seconds
+void go_red(){
+  	digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    delay(5000);
+}
+
+
+// green off, yellow on for 3 seconds
+void go_yellow(){
+  	digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    delay(3000);
+}
+
+
+// red and yellow on for 3 seconds (red is already on though)
+void go_yellow_red(){
+	digitalWrite(yellow, HIGH);
+    delay(3000);
 }
