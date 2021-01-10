310dcd25ce949405535fe59fdc8aadeb9ef25cd0
CkOmit <72194591+CkOmit@users.noreply.github.com>
2021-01-10 10:26:06 +0100

    Add files via upload

---
+++ Project_8/SmartHomeProject/SmartHomeProject/window.cpp
@@ -0,0 +1,23 @@
+#include "window.h"
+
+Window :: Window (/ * args * /)
+{
+  }
+Window :: ~ Window ()
+{
+  }
+void Window :: open () {
+  status = Status :: open;
+}
+
+  void Window :: close  () {
+    status = Status :: close;
+  }
+
+  void Window :: semiopen () {
+      status = Status :: semiopen;
+    }
+    Status Window::returnStatus(){
+      return status;
+
+    }
