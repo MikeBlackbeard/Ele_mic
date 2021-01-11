e7a95fc37ccf7cfae5e296bda8b3df8360209c31
Charles Okere <okerecharles85@gmail.com>
2021-01-11 12:59:10 +0100

    Create Attemptoopen.cpp
    Attempt To Open Added

---
+++ Project_8/SD2/SD2/Attemptoopen.cpp
@@ -0,0 +1,47 @@
+#include <iostream
+//#include "visitor.h
+
+#define CONFIRM_UNKNOWN 0
+#define ACTIVATE_ALARM_SYSTEM 1
+#define VISITOR 2
+#define EXIT 3
+
+confirmStatus - CONFIRM_UNKNOWN;
+
+using namespace std;
+
+int confirmUnknown()
+{
+      cout <<"New open attempt detected. do you want to activate the alarm system? (Y/N)";
+      char input;
+      cit >> input;
+      if (input == 'y'|| input == 'Y')
+        return ACTIVATE_ALARM_SYSTEM;
+      else
+            return VISITOR;
+}
+
+
+
+int main ()
+{
+    switch (confirmStatus)
+    {
+    case CONFIRM_UNKNOWN:
+        confirmStatus = confirmUnknown();
+        break;
+    case ACTIVATE_ALARM_SYSTEM:
+         cout <<" The alarm system is activated. ";
+         confirmStatus = EXIT;
+         break;
+         case VISITOR:
+         cout << " Sending to Visitor.";
+         visitor.h
+         confirmStatus = EXIT;
+         break;
+    default:
+        break;
+    }
+}        
+
+        
